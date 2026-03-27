0cc935ac9781b9fcb2006c8273ddd51c1e60eb4c
RitekPillai <mugiwaradluffy102@gmail.com>
2026-03-27 11:22:05 +0530

    linear layout

---
+++ LinearLayout/app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.linearlayout;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lv);
+        tv=findViewById(R.id.tv);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+       lv.setOnItemClickListener((parent,view,position,id)->{
+       String item = parent.getItemAtPosition(position).toString();tv.setText(item);
+       });
+    }
+}
